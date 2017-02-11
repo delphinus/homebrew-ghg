c8253e1d055d301c072e0935c4cc8a3dfccab848
delphinus <delphinus@remora.cx>
2017-02-11 16:22:59 +0900

    Commit at first

---
+++ ghg.rb
@@ -0,0 +1,22 @@
+class Ghg < Formula
+  desc "Get the executable from github releases"
+  homepage "https://github.com/Songmu/ghg"
+  url "https://github.com/Songmu/ghg/archive/v0.0.3.tar.gz"
+  sha256 "19606c6d24d4fb2621fc6cae8f9c8f336b7de318ace984a8da37beff732c91d5"
+  head "https://github.com/Songmu/ghg"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/Songmu/ghg").install buildpath.children
+    cd "src/github.com/Songmu/ghg" do
+      system "go", "build", "-o", bin/"ghg", "cmd/ghg/ghg.go"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    system "#{bin}/ghg", "--version"
+  end
+end
